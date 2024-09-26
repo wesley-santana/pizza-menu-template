56a976e526b0ac1896f7ecd9b033d7c25c2625ba
Wesley de Santana <wesleysantana_gabriel@outlook.com>
2024-09-26 16:45:07 -0300

    chore: google search console

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   ssr: false,
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'google-site-verification',
+          content: 'iRM2mAsobIRkCayqeOJBlXglOgW5hqJhMTUP7xS3FG4'
+        }
+      ]
+    }
+  }
 })
